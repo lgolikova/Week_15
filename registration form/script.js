eb2d9620d7d1ae9eb8aa4fcece02e948e32cce05
lgolikova <milagoul@gmail.com>
2022-05-13 12:20:43 +0300

    Удаление лишних строк

--- registration form/script.js
+++ registration form/script.js
@@ -13,54 +13,43 @@ function checkForm() {
     }
     else {
         name.style.color = "black";
-        
         }
-
-        if (surname.value == "") {
-            surname.style.color = "red";
-            surname.value = "Введите вашу фамилию";
-            counter+=1;
+    if (surname.value == "") {
+        surname.style.color = "red";
+        surname.value = "Введите вашу фамилию";
+        counter+=1;
+    }
+    else {
+        surname.style.color = "black";
+    }
+    if (email.value == "") {
+        email.type = "text";
+        email.value = "Введите email";
+        email.style.color = "red";
+        counter+=1;
+    }
+    else {
+        email.style.color = "black";
+    }
+    if (password.value == "") {
+        password.type = "text";
+        password.value = "Введите пароль";
+        password.style.color = "red";  
+        counter+=1;
         }
-        else {
-            surname.style.color = "black";
-            
+    else if (password.value.length <= 6) {
+        password.type = "text";
+        password.value = "Пароль слишком короткий";
+        password.style.color = "red";
+        counter+=1;
         }
-
-        if (email.value == "") {
-            email.type = "text";
-            email.value = "Введите email";
-            email.style.color = "red";
-            counter+=1;
-        }
-        else {
-            email.style.color = "black";
-            
-        }
-
-        if (password.value == "") {
-            password.type = "text";
-            password.value = "Введите пароль";
-            password.style.color = "red";  
-            counter+=1;
-        }
-        // else {
-        //     password.style.color = "black";
-            
-        // }
-        else if (password.value.length <= 6) {
-            password.type = "text";
-            password.value = "Пароль слишком короткий";
-            password.style.color = "red";
-            counter+=1;
-        }
-        else {
-            password.style.color = "black";
-            
-        }
-        if (counter==0) {
-            let name1 = name.value;
-            alert (`Привет, ${name1}!`);
-        }
+    else {
+        password.style.color = "black";
+    }
+    if (counter==0) {
+        let name1 = name.value;
+        alert (`Привет, ${name1}!`);
+    }
 }
     
     
